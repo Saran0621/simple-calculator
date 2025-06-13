4646129b466c70e1ab69fade20f4dacb317bd06e
Saran0621 <ballasaran@gmail.com>
2025-06-13 23:03:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  if (display.innerText === "0" && value !== ".") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function backspace() {
+  display.innerText = display.innerText.slice(0, -1) || "0";
+}
+
+function calculate() {
+  try {
+    let result = eval(display.innerText);
+    display.innerText = result;
+  } catch (e) {
+    display.innerText = "Error";
+  }
+}
